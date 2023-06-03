f3bb98b465d231a9d89dfedc2d0c8f8c2d63fd91
Freddy23245 <cyberdyne710@gmail.com>
2023-06-03 12:38:21 -0300

    Se esta haciendo el modulo ventas y pedidos se agrego codigo ajax

--- SistemaPedidos/SistemaPedidos/Controllers/VentasController.cs
+++ SistemaPedidos/SistemaPedidos/Controllers/VentasController.cs
@@ -17,16 +17,32 @@ namespace SistemaPedidos.Controllers
             var vent = await context.Venta.Include(x => x.IdClienteNavigation).Include(t=>t.IdTipoNavigation).ToListAsync();
             Ventas xvent = new Ventas();          
             ViewBag.Ventas = vent;
-            var cust = await context.Clientes.ToListAsync();
-            var tipo = await context.Tipos.ToListAsync();
-            ViewBag.Tipos = tipo;
-            ViewBag.Clientes = cust;
+        
             //Tuple<Ventas, Pedido> Model = new Tuple<Ventas, Pedido>(Venta, new Pedido());
             return View(xvent);
         }
         [BindProperty]
         public Ventas venta { get; set; }
-        public async Task<IActionResult> Registrar()
+        public async Task<IActionResult> Nuevo(int? id)
+        {
+            var _vent = await context.Venta.FindAsync(id);
+            var Venta = new Ventas();
+            if (_vent != null)
+            {
+                Venta = _vent;
+            }
+            var cust = await context.Clientes.ToListAsync();
+            var tipo = await context.Tipos.ToListAsync();
+            ViewBag.Tipos = tipo;
+            ViewBag.Clientes = cust;
+            ViewBag.Estado = await context.Estados.ToListAsync();
+            ViewBag.Productos = await context.Productos.Select(x => new { x.IdProducto, NombreProducto = x.Nombre + " - " + x.IdMarcaNavigation.Nombre }).ToListAsync();
+            Tuple<Ventas, Pedido> Model = new Tuple<Ventas, Pedido>(Venta, new Pedido());
+
+            return View(Model);
+        }
+
+        public async Task<IActionResult> Registrar( int? id)
         {
             var vent = await context.Venta.Where(x=> x.IdVenta == venta.IdVenta).AnyAsync();
 
@@ -40,7 +56,17 @@ namespace SistemaPedidos.Controllers
             await context.SaveChangesAsync();
             return RedirectToAction("Nuevo.cshtml", "Pedidos");
         }
-
+        [HttpGet]
+        public async Task<IActionResult>TraerProducto(int id)
+        {
+            var producto = await context.Productos.FirstOrDefaultAsync(p => p.IdProducto == id);
+            if(producto == null)
+                return NotFound();
+            else
+            {
+                return Ok(producto);
+            }
+        }
         public async Task<IActionResult> Eliminar(int id)
         {
             var VentDelete = await context.Venta.FindAsync(id);
---
+++ SistemaPedidos/SistemaPedidos/wwwroot/js/TraerProductos.js
@@ -0,0 +1,16 @@
+﻿$("#select-productos").change(function () {
+    var idProducto = $(this).val();
+    $.ajax({
+        url: "/Ventas/TraerProducto/",
+        type: "GET",
+        data: { id: idProducto },
+        success: function (data) {
+            console.log(data);
+            $("#StockProd").val(data.stock);
+            $("#PrecioProd").val(data.precio);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
